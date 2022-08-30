ac297bb930149a8c0998bff0149af29346d09eb6
ukihot <tokunaga.y@tyco.co.jp>
2022-08-30 17:51:41 +0900

    wip

--- src/pages/Mattia.tsx
+++ src/pages/Mattia.tsx
@@ -4,6 +4,12 @@ import { Component } from "solid-js";
 const Mattia: Component = () => {
   return (
     <>
+      <p>えいぎょうじかん</p>
+      <p>えいぎょうじかん</p>
+      <p>えいぎょうじかん</p>
+      <p>えいぎょうじかん</p>
+      <p>えいぎょうじかん</p>
+      <p>えいぎょうじかん</p>
       <Link href="/liveschedule">→らいぶすけじゅーる←</Link>
       <p>下はまっぷ</p>
     </>
